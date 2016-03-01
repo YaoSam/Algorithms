716b7aafe1a62874df1216c06b9f983feb2af49e
SamYao <yaosam@yeah.net>
2016-03-01 22:30:50 +0800

    不知道这是什么鬼……

---
+++ SbPicture/main.cpp
@@ -0,0 +1,67 @@
+/*
+*　　　　　　　　┏┓　　　┏┓+ +
+*　　　　　　　┏┛┻━━━┛┻┓ + +
+*　　　　　　　┃　　　　　　　┃ 　
+*　　　　　　　┃　　　━　　　┃ ++ + + +
+*　　　　　　 ████━████ ┃+
+*　　　　　　　┃　　　　　　　┃ +
+*　　　　　　　┃　　　┻　　　┃
+*　　　　　　　┃　　　　　　　┃ + +
+*　　　　　　　┗━┓　　　┏━┛
+*　　　　　　　　　┃　　　┃　　　　　　　　　　　
+*　　　　　　　　　┃　　　┃ + + + +
+*　　　　　　　　　┃　　　┃　　　　Code is far away from bug with the animal protecting　　　　　　　
+*　　　　　　　　　┃　　　┃ + 　　　　神兽保佑,代码无bug　　
+*　　　　　　　　　┃　　　┃
+*　　　　　　　　　┃　　　┃　　+　　　　　　　　　
+*　　　　　　　　　┃　 　　┗━━━┓ + +
+*　　　　　　　　　┃ 　　　　　　　┣┓
+*　　　　　　　　　┃ 　　　　　　　┏┛
+*　　　　　　　　　┗┓┓┏━┳┓┏┛ + + + +
+*　　　　　　　　　　┃┫┫　┃┫┫
+*　　　　　　　　　　┗┻┛　┗┻┛+ + + +
+*/
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <algorithm>
+using namespace std;
+#undef re
+#define re(i,n) for(unsigned int i=0;i<n;i++)
+#define DEBUG
+
+void print(unsigned int n,unsigned int k=0)
+{
+	static string str;
+	if (n == 0)return;
+	if (n % 2 == 0)
+		print(n / 2, k + 1);
+	else
+	{
+		str.assign((n/2)*(1<<k), ' ');
+		str.append(1<<k, '*');
+		cout << str << endl;
+	}
+}
+
+int main()
+{
+	clock_t BeginTime = clock();
+	srand(unsigned(time(NULL)));
+#ifdef DEBUG
+	FILE *input, *output; //没用的指针... ...
+	freopen_s(&input, "in.txt", "r", stdin);
+	freopen_s(&output, "out.txt", "w", stdout);
+#endif
+	try{
+		int n;
+		cin >> n;
+		re(i,n)
+			print(i+1);
+	}
+	catch (const char * error){ cout << error << endl; }
+#ifndef DEBUG
+	system("pause");
+#endif // !DEBUG
+	return 0;
+}
