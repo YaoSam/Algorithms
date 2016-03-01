d50f33ce5978966fabde9fa0e60dc8b72033cd96
SamYao <yaosam@yeah.net>
2016-03-01 20:12:33 +0800

    抄了书本的排列组合。递归做的。没什么意思

---
+++ Permutation/main.cpp
@@ -0,0 +1,77 @@
+/*
+*　　　　　　　　┏┓　　　┏┓+ +
+*　　　　　　　┏┛┻━━━┛┻┓ + +
+*　　　　　　　┃　　　　　　　┃ 　
+*　　　　　　　┃　　　━　　　┃ ++ + + +
+*　　　　　　 ████━████ ┃+
+*　　　　　　　┃　　　　　　　┃ +
+*　　　　　　　┃　　　┻　　　┃
+*　　　　　　　┃　　　　　　　┃ + +
+*　　　　　　　┗━┓　　　┏━┛
+*　　　　　　　　　┃　　　┃　　　　　　　　　　　
+*　　　　　　　　　┃　　　┃ + + + +
+*　　　　　　　　　┃　　　┃　　　　Code is far away from bug with the animal protecting　　　　　　　
+*　　　　　　　　　┃　　　┃ + 　　　　神兽保佑,代码无bug　　
+*　　　　　　　　　┃　　　┃
+*　　　　　　　　　┃　　　┃　　+　　　　　　　　　
+*　　　　　　　　　┃　 　　┗━━━┓ + +
+*　　　　　　　　　┃ 　　　　　　　┣┓
+*　　　　　　　　　┃ 　　　　　　　┏┛
+*　　　　　　　　　┗┓┓┏━┳┓┏┛ + + + +
+*　　　　　　　　　　┃┫┫　┃┫┫
+*　　　　　　　　　　┗┻┛　┗┻┛+ + + +
+*/
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <algorithm>
+using namespace std;
+#undef re
+#define re(i,n) for(unsigned int i=0;i<n;i++)
+#define DEBUG
+
+template <class T>
+void Swap(T & a, T &b){
+	T temp = a; a = b; b = temp;
+}
+
+
+void Permutation(int *a, int k, int m){
+	if (k == m){
+		re(i, m)
+			cout << a[i] << " ";
+		cout << endl;
+	}
+	else
+	{
+		for (int i = k; i < m; i++){
+			Swap(a[k], a[i]);
+			Permutation(a, k + 1, m);
+			Swap(a[k], a[i]);
+		}
+	}
+}
+
+int main()
+{
+	clock_t BeginTime = clock();
+	srand(unsigned(time(NULL)));
+#ifdef DEBUG
+	FILE *input, *output; //没用的指针... ...
+	freopen_s(&input, "in.txt", "r", stdin);
+	freopen_s(&output, "out.txt", "w", stdout);
+#endif
+	try{
+		int a[100];
+		int n;
+		cin >> n;
+		re(i, n)
+			a[i] = i+1;
+		Permutation(a, 0, n);
+	}
+	catch (const char * error){ cout << error << endl; }
+#ifndef DEBUG
+	system("pause");
+#endif // !DEBUG
+	return 0;
+}
