3da5c21127b3bae826776f3853cb5678de292730
SamYao <yaosam@yeah.net>
2016-03-31 09:04:14 +0800

    在Algorithms加了个模板

---
+++ main.cpp
@@ -0,0 +1,50 @@
+/*
+*　　　　　　　　┏┓　　　┏┓+ +
+*　　　　　　　┏┛┻━━━┛┻┓ + +
+*　　　　　　　┃　　　　　　　┃ 　
+*　　　　　　　┃　　　━　　　┃ ++ + + +
+*　　　　　　 ████━████ ┃+
+*　　　　　　　┃　　　　　　　┃ +
+*　　　　　　　┃　　　┻　　　┃
+*　　　　　　　┃　　　　　　　┃ + +
+*　　　　　　　┗━┓　　　┏━┛
+*　　　　　　　　　┃　　　┃　　　　　　　　　　　
+*　　　　　　　　　┃　　　┃ + + + +
+*　　　　　　　　　┃　　　┃　　　　Code is far away from bug with the animal protecting　　　　　　　
+*　　　　　　　　　┃　　　┃ + 　　　　神兽保佑,代码无bug　　
+*　　　　　　　　　┃　　　┃
+*　　　　　　　　　┃　　　┃　　+　　　　　　　　　
+*　　　　　　　　　┃　 　　┗━━━┓ + +
+*　　　　　　　　　┃ 　　　　　　　┣┓
+*　　　　　　　　　┃ 　　　　　　　┏┛
+*　　　　　　　　　┗┓┓┏━┳┓┏┛ + + + +
+*　　　　　　　　　　┃┫┫　┃┫┫
+*　　　　　　　　　　┗┻┛　┗┻┛+ + + +
+*/
+#include <iostream>
+#include <time.h>
+using namespace std;
+#define DEBUG
+#undef re
+#define re(i,n) for(unsigned int i=0;i<n;i++)
+
+
+int main()
+{
+	clock_t BeginTime = clock();
+	srand(unsigned(time(nullptr)));
+#ifdef DEBUG
+	FILE *input, *output; //没用的指针... ...
+	freopen_s(&input, "in.txt", "r", stdin);
+	freopen_s(&output, "out.txt", "w", stdout);
+#endif
+
+	/*主函数体*/
+
+
+	cout << "运行时间：" << (clock() - BeginTime) << endl;
+#ifndef DEBUG
+	system("pause");
+#endif // !DEBUG
+	return 0;
+}
