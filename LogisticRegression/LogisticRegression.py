59b14174b5aa1e0d59c44e91dbcb28e8ebe23ba9
SamYao <yaosam@yeah.net>
2016-05-11 18:35:22 +0800

    update

--- LogisticRegression/LogisticRegression.py
+++ LogisticRegression/LogisticRegression.py
@@ -69,10 +69,8 @@ def stocGradAscent1(dataMatrix, classLabels, numIter=150):
     for j in range(numIter):
         dataIndex = range(m)
         for i in range(m):
-            alpha = 4/(1.0+j+i)+0.0001   
-#apha decreases with iteration, does not 
+            alpha = 4/(1.0+j+i)+0.0001 #apha decreases with iteration, does not go to 0 because of the constant
             randIndex = int(random.uniform(0,len(dataIndex)))
-#go to 0 because of the constant
             h = sigmoid(sum(dataMatrix[randIndex]*weights))
             error = classLabels[randIndex] - h
             weights = weights + alpha * error * dataMatrix[randIndex]    
