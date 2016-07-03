8868a960b9d45b0ebfa4702c1e9b016a72387061
SamYao <yaosam@yeah.net>
2016-07-03 18:03:31 +0800

    Revert "update"
    This reverts commit 098605d1ba1b4d463daab4935a59fc8a5082127c.

--- InsertSort/main.cpp
+++ InsertSort/main.cpp
@@ -25,12 +25,48 @@
 #include <string>
 #include <time.h>
 #include <algorithm>
+#include "../Data Sturcture/normal.cpp"
 using namespace std;
 #undef re
 #define re(i,n) for(unsigned int i=0;i<n;i++)
 #define DEBUG
 
+template<class T>
+void InsertSort(T* arr, unsigned int num)
+{
+	static T key;
+	static int i;
+	for (int j = 0; j < num; j++)
+	{
+		key = arr[j];
+		i = j - 1;
+		while (i >= 0 && arr[i]>key)
+		{
+			arr[i + 1] = arr[i];
+			i--;
+		}
+		arr[i + 1] = key;
+	}
+}
 
+
+TEMP
+void Qsort(T a[], int left, int right)
+{
+	T mid = a[(left + right) / 2];
+	int l = left, r = right;
+	do
+	{
+		while (a[l] < mid)l++;
+		while (a[r] > mid)r--;
+		if (l <= r)
+			swap(a[l++], a[r--]);
+	} while (l <= r);
+	if (r > left)		Qsort(a, left, r);
+	if (l < right)	Qsort(a, l, right);
+}
+
+int arr[100000000], brr[100000000];
 int main()
 {
 	clock_t BeginTime = clock();
@@ -42,8 +78,24 @@ int main()
 #endif
 	try {
 		/*Ö÷º¯Êý*/
-
-
+		unsigned int TestSize = 1000000;
+		re(i, TestSize)
+			arr[i] = brr[i] = rand() % TestSize;
+		//InsertSort(arr, TestSize);
+		clock_t a = clock();
+		sort(brr, brr + TestSize);
+		cout << clock() - a << endl;
+		a = clock();
+		Qsort(arr, 0, TestSize - 1);
+		cout << clock() - a << endl;
+		//re(i, TestSize)
+		//	cout << arr[i] << " ";
+		//cout << endl;
+		//re(i, TestSize)
+		//	cout << brr[i] << " ";
+		cout << endl;
+		if (!equal(arr, arr + TestSize, brr))
+			cout << "That is NOT EQUAL!!!" << endl;
 
 	}
 	catch (const char * error) { cout << error << endl; }
